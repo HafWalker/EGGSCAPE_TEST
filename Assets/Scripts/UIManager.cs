17d9df48313749d8f8522890afbb0ddc1b67fb38
Hernan Ferrari <hernan.angel.ferrari@gmail.com>
2024-03-14 14:49:06 -0300

    Added UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<UIManager>();
+                if (_instance == null)
+                {
+                    GameObject singleton = new GameObject("GameManager");
+                    _instance = singleton.AddComponent<UIManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public Slider firstPersonHealthSlider;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+}
